e9c7ea452b2d0ba1554f94e25fcde880330be437
Tony Robertson <tonyr729@gmail.com>
2018-07-16 13:09:26 -0600

    Add production to knex for heroku deployment.

---
+++ knexfile.js
@@ -0,0 +1,22 @@
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/mars_list',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
+    useNullAsDefault: true
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
+  }
+};
