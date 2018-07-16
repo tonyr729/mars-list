fe74a5a39146b1362fa76cce57244f862f8bf087
Tony Robertson <tonyr729@gmail.com>
2018-07-16 10:22:19 -0600

    Add endpoints to server to access db

--- server.js
+++ server.js
@@ -13,11 +13,44 @@ app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Mars List';
 
 app.get('/api/v1/items', (request, response) => {
-  
+  database('items').select()
+    .then(items => {
+      response.status(200).json(items);
+    })
+    .catch((error) => {
+      response.status(500).json({ error });
+    });
 });
 
-app.get('/api/v1/items/:id', (request, response) => {
-  
+app.post('/api/v1/items', (request, response) => {
+  const item = request.body;
+  const itemParams = Object.keys(item)
+  const requiredParameter = ['name', 'isPacked']
+
+  if (!itemParams === requiredParams) {
+    return response
+      .status(422)
+      .send({ error: `Expected format: { name: <String>, isPacked: <Boolean> }. You're missing a "${requiredParameter}" property.` });
+  }
+
+  database('items').insert(item, 'id')
+    .then(itemID => {
+      response.status(201).json({ id: itemID[0] })
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+});
+
+app.delete('/api/v1/items/:id', (request, response) => {
+  const { id } = request.params
+  database('items').where('id', id).del()
+    .then(() => {
+      response.sendStatus(204)
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
 });
 
 app.listen(app.get('port'), () => {
